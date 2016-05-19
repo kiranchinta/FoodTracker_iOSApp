a3fe4356f952ccafe9a1a779bef34286772c0e65
kiranchinta <kiranchinta@gmail.com>
2016-05-18 17:20:02 -0700

    staging work

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func setDefaultTextLabelText(sender: UIButton) {
     }
 
 
